b99860f3dffb8c5cde7f61b2a3814471c6394fe8
Damon Cai <20635117+Oneiriac@users.noreply.github.com>
2020-11-14 02:34:09 +1100

    Add different scales to use
    Improve documentation

--- src/music.cpp
+++ src/music.cpp
@@ -4,49 +4,111 @@
 #include <math.h>
 
 #include <cmath>
+#include <iterator>
 #include <string>
+#include <vector>
 
+using namespace std;
+
+// How many centimetres away from the last note transition point before a transition is allowed
 #define HYSTERESIS_THRESHOLD 2
-#define BASE_NOTE_BUCKET_OFFSET 2  // How many extra buckets should capture the base note
+// How many extra buckets should be allocated to the base note (increasing the range of distances for the base note)
+#define BASE_NOTE_BUCKET_OFFSET 2
 
 double ratio = pow(2.0, 1.0 / 12.0);
 float lastTransitionDistance;
 int currentNote = 0;
 
-std::string noteStrings[] = {
+vector<int> MAJOR_SCALE = {
+    0, 2, 4, 5, 7, 9, 11,
+};
+
+vector<int> PENTATONIC_SCALE = {
+    0, 2, 4, 7, 9,
+};
+
+vector<int> MINOR_SCALE = {0, 2, 3, 5, 7, 8, 10};
+vector<int> WHOLE_TONE_SCALE = {0, 2, 4, 6, 8, 10};
+
+#define SCALE_TO_USE MINOR_SCALE
+
+/**
+ * @brief From the note number (i.e. notes above a root note in the given scale), calculate the chromatic note number
+ * (i.e. notes above the root note in the chromatic scale).
+ *
+ * @param scale
+ * @param noteNumber
+ * @return int
+ */
+int chromatic_note_number(vector<int> scale, int noteNumber) {
+  int octavesAbove = noteNumber / scale.size();
+  int baseChromaticNote = scale[noteNumber % scale.size()];
+  return octavesAbove * 12 + baseChromaticNote;
+}
+
+string noteStrings[] = {
     "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B",
 };
 
-std::string frequency_to_note(float frequency) {
-  int midiNote = (12 * (log10(frequency / 440.0) / log10(2)) + 57) + 0.5;
+/**
+ * @brief Get the note letter from the frequency (snapped to the nearest whole tone).
+ *
+ * @param frequency
+ * @return string
+ */
+string frequency_to_note(float frequency) {
+  int midiNote = (12 * log2(frequency / 440.0) + 57) + 0.5;
   return noteStrings[midiNote % 12];
 }
 
-int ordinal_note_from_distance(float distance, float maxDistance, int rangeSize) {
+/**
+ * @brief Calculate the note number.
+ *
+ * @param distance
+ * @param maxDistance
+ * @param rangeSize
+ * @return int
+ */
+int note_number_from_distance(float distance, float maxDistance, int rangeSize) {
   int numBuckets = rangeSize + BASE_NOTE_BUCKET_OFFSET;
   int thisBucket = round(static_cast<double>(numBuckets * (maxDistance - distance) / maxDistance));
-  int ordinalNote = max(thisBucket - BASE_NOTE_BUCKET_OFFSET, 0);
-  return ordinalNote;
+  return max(thisBucket - BASE_NOTE_BUCKET_OFFSET, 0);
 }
 
-double frequency_from_base(double baseFrequency, double notesAboveBase) {
-  return baseFrequency * pow(2.0, notesAboveBase / 12.0);
+/**
+ * @brief Caculate the frequency from the base/root frequency and note number.
+ *
+ * @param baseFrequency
+ * @param noteNumber
+ * @return double
+ */
+double calc_frequency(double baseFrequency, int noteNumber) {
+  return baseFrequency * pow(2.0, (double)chromatic_note_number(SCALE_TO_USE, noteNumber) / 12.0);
 }
 
+/**
+ * @brief Calculate frequency (from a minFrequency) using a HC-SR04.
+ *
+ * @param distance
+ * @param maxDistance
+ * @param minFrequency
+ * @param rangeSize
+ * @return float
+ */
 float frequency_from_distance(float distance, float maxDistance, double minFrequency, int rangeSize) {
   boolean insideThreshold = std::abs(distance - lastTransitionDistance) <= HYSTERESIS_THRESHOLD;
   boolean noDistance = distance == 0;
   // HC-SR04 returns too many invalid out-of-range (distance = 0): when this happens, stop the frequency from changing
   if (insideThreshold || noDistance) {
     Serial.println(distance);
-    return frequency_from_base(minFrequency, currentNote);
+    return calc_frequency(minFrequency, currentNote);
   }
-  int notesAboveBase = ordinal_note_from_distance(distance, maxDistance, rangeSize);
-  if (notesAboveBase != currentNote) {
-    currentNote = notesAboveBase;
+  int noteNumber = note_number_from_distance(distance, maxDistance, rangeSize);
+  if (noteNumber != currentNote) {
+    currentNote = noteNumber;
     lastTransitionDistance = distance;
   }
-  float frequency = frequency_from_base(minFrequency, notesAboveBase);
+  float frequency = calc_frequency(minFrequency, noteNumber);
   char output[96];
   snprintf(output, sizeof(output), "distance: %.2f\t\tlastTransitionDistance: %.2f\t\tnote: %s", distance,
            lastTransitionDistance, frequency_to_note(frequency).c_str());
@@ -58,6 +120,13 @@ float gainArray[10] = {0};
 int gainArraySize = sizeof(gainArray) / sizeof(float);
 int currentIndex = 0;
 
+/**
+ * @brief Calculate gain (0.0-1.0) using a HC-SR04.
+ *
+ * @param distance
+ * @param maxDistance
+ * @return float
+ */
 float gain_from_distance(float distance, float maxDistance) {
   float thisVolume = static_cast<float>((maxDistance - distance) / maxDistance);
   gainArray[currentIndex] = thisVolume;
