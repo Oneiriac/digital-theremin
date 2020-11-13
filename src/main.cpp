2783de159579fe610c21e3341a22889f85ec4cc6
Damon Cai <20635117+Oneiriac@users.noreply.github.com>
2020-11-14 01:04:03 +1100

    Create more complex sound with multiple waveforms

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,26 @@
 #include "wait.h"
 
 // GUItool: begin automatically generated code
-AudioSynthWaveform waveform1;  // xy=450.0056686401367,191.99994659423828
-AudioAmplifier amp1;           // xy=745.0056076049805,175.005615234375
-AudioOutputI2S i2s1;           // xy=1010.0057220458984,184.99980354309082
-AudioConnection patchCord1(waveform1, amp1);
-AudioConnection patchCord2(amp1, 0, i2s1, 0);
-AudioConnection patchCord3(amp1, 0, i2s1, 1);
+AudioSynthWaveformSine sine1;              // xy=489.0056266784668,603.0000200271606
+AudioSynthWaveform waveform1;              // xy=663.0056076049805,451.99992752075195
+AudioSynthWaveformModulated waveformMod1;  // xy=664.0056304931641,558.0000143051147
+AudioSynthWaveform waveform2;              // xy=665.4602279663086,499.9147253036499
+AudioSynthNoisePink pink1;                 // xy=688.0056610107422,653.000020980835
+AudioMixer4 mixer1;                        // xy=902.0054550170898,450.9999313354492
+AudioEffectFreeverb freeverb1;             // xy=1100.0056838989258,399.99995613098145
+AudioAmplifier amp1;                       // xy=1386.0057678222656,387.9999465942383
+AudioFilterStateVariable filter1;          // xy=1578.8239822387695,380.99988555908203
+AudioOutputI2S i2s1;                       // xy=1799.0056610107422,367.9999599456787
+AudioConnection patchCord1(sine1, 0, waveformMod1, 0);
+AudioConnection patchCord2(waveform1, 0, mixer1, 0);
+AudioConnection patchCord3(waveformMod1, 0, mixer1, 1);
+AudioConnection patchCord4(waveform2, 0, mixer1, 3);
+AudioConnection patchCord5(pink1, 0, mixer1, 2);
+AudioConnection patchCord6(mixer1, freeverb1);
+AudioConnection patchCord7(freeverb1, amp1);
+AudioConnection patchCord8(amp1, 0, filter1, 0);
+AudioConnection patchCord9(filter1, 0, i2s1, 0);
+AudioConnection patchCord10(filter1, 0, i2s1, 1);
 // GUItool: end automatically generated code
 
 #define TRIGGER_PIN 12   // Arduino pin tied to trigger pin on the ultrasonic sensor.
@@ -29,11 +43,32 @@ AudioConnection patchCord3(amp1, 0, i2s1, 1);
 NewPing sonar(TRIGGER_PIN, ECHO_PIN,
               MAX_DISTANCE);  // NewPing setup of pins and maximum distance.
 
+void play_frequency(float freq) {
+  waveform1.frequency(freq / 2.0);
+  waveformMod1.frequency(freq);
+  waveform2.frequency(freq * 1.5);
+}
+
 void setup() {
   AudioMemory(10);
   Serial.begin(115200);
-  waveform1.begin(WAVEFORM_SINE);
-  waveform1.amplitude(1.0);
+  // Configure osc 1
+  waveform1.begin(WAVEFORM_SQUARE);
+  waveform1.amplitude(0.1);
+  // Configure osc 2
+  waveformMod1.begin(WAVEFORM_SINE);
+  waveformMod1.amplitude(0.2);
+  // Configure osc 3
+  waveform2.begin(WAVEFORM_TRIANGLE);
+  waveform2.amplitude(0.25);
+  sine1.frequency(0.5);
+  sine1.amplitude(0.1);
+  waveformMod1.frequencyModulation(1.0 / 192.0);
+  // Misc
+  pink1.amplitude(0.2);
+  freeverb1.roomsize(0.2);
+  freeverb1.damping(0.1);
+  filter1.frequency(8 * MIN_FREQ);
   amp1.gain(1.0);
 }
 
@@ -45,7 +80,7 @@ void loop() {
   // Frequency: lower the further away you get, higher the closer you get
   float frequency = frequency_from_distance(distance, MAX_DISTANCE, MIN_FREQ, RANGE_SIZE);
   float gain = gain_from_distance(distance, MAX_DISTANCE);  // Gain: lower when further away, higher when closer
-  waveform1.frequency(frequency);
+  play_frequency(frequency);
   // amp1.gain(gain);
   // Print output to Serial
   char output[64];
