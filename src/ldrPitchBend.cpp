5dca616dc9bd2b639d519303fb81da2d9ad8bfdb
Damon Cai <20635117+Oneiriac@users.noreply.github.com>
2020-11-17 16:03:14 +1100

    Add max bound for pitch bend readings

--- src/ldrPitchBend.cpp
+++ src/ldrPitchBend.cpp
@@ -6,14 +6,15 @@
 
 #include "rescale.h"
 
-#define ZERO_RANGE 0.15F
+#define PITCH_BEND_MIN_BOUND 0.15F
+#define PITCH_BEND_MAX_BOUND 0.9F
 #define PITCH_BEND_SCALE_FACTOR 10.0F
 
 #define NUM_PITCH_BENDS_TO_AVERAGE 5
 int pitchBendsToAverage[NUM_PITCH_BENDS_TO_AVERAGE] = {0};
 unsigned int pitchBendIndex = 0;
 
-float clip(float n, float lower, float upper) { return max(lower, min(n, upper)); }
+double clip(double n, double lower, double upper) { return max(lower, min(n, upper)); }
 
 float read_pitch_bend(unsigned int bend_up_pin, unsigned int bend_down_pin) {
   // Read LDRs for pitch bend
@@ -28,20 +29,29 @@ float read_pitch_bend(unsigned int bend_up_pin, unsigned int bend_down_pin) {
   for (int i = 0; i < NUM_PITCH_BENDS_TO_AVERAGE; i++) {
     sum += pitchBendsToAverage[pitchBendIndex];
   }
-  float pitchBend =
-      clip(PITCH_BEND_SCALE_FACTOR * (float)sum / (1024.0 * (float)NUM_PITCH_BENDS_TO_AVERAGE), -1.0, +1.0);
+  double pitchBend = PITCH_BEND_SCALE_FACTOR * (double)sum / (1024.0 * (double)NUM_PITCH_BENDS_TO_AVERAGE);
   // Rescale pitch bend using linear interpolation
-  float rescaledPitchBend;
-  // Use std::abs, otherwise will be cast to int
-  if (std::abs(pitchBend) <= ZERO_RANGE) {
-    // Values within [-ZERO_RANGE, ZERO_RANGE] are set to zero to stop fluctuations
-    rescaledPitchBend = 0.0;
-  } else if (pitchBend > ZERO_RANGE) {
-    // Rescale values from (ZERO_RANGE, 1.0] to (0.0, 1.0]
-    rescaledPitchBend = rescale(ZERO_RANGE, 1.0, 0.0, 1.0, pitchBend);
+  double rescaledPitchBend;
+  if (std::abs(pitchBend) <= PITCH_BEND_MIN_BOUND) {  // Use std::abs, otherwise will be cast to int
+    rescaledPitchBend =
+        0.0;  // Values within [-PITCH_BEND_MIN_BOUND, +PITCH_BEND_MIN_BOUND] are set to zero to stop fluctuations
+
   } else {
-    // Rescale values from [-1.0, ZERO_RANGE) to [-1.0, 0.0)
-    rescaledPitchBend = rescale(-1.0, -ZERO_RANGE, -1.0, 0.0, pitchBend);
+    double oldMin, oldMax, newMin, newMax;
+    if (pitchBend > 0) {
+      // Rescale values from (+PITCH_BEND_MIN_BOUND, 1.0] to (0.0, 1.0]
+      oldMin = PITCH_BEND_MIN_BOUND;
+      oldMax = PITCH_BEND_MAX_BOUND;
+      newMin = 0.0;
+      newMax = 1.0;
+    } else {
+      // Rescale values from [-1.0, -PITCH_BEND_MIN_BOUND) to [-1.0, 0.0)
+      oldMin = -PITCH_BEND_MAX_BOUND;
+      oldMax = -PITCH_BEND_MIN_BOUND;
+      newMin = -1.0;
+      newMax = 0.0;
+    }
+    rescaledPitchBend = clip(rescale(oldMin, oldMax, newMin, newMax, pitchBend), newMin, newMax);
   }
   return rescaledPitchBend;
 }
