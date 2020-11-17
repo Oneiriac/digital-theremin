c4363fc05b47abf37e0fa9838786637159afc252
Damon Cai <20635117+Oneiriac@users.noreply.github.com>
2020-11-17 14:53:52 +1100

    LDR pitch bend averaging, dead zone around 0.0 to stop fluctuations

---
+++ src/ldrPitchBend.h
@@ -0,0 +1,7 @@
+#ifndef LDR_PITCH_BEND_H
+#define LDR_PITCH_BEND_H
+// TODO: set min/max range using button press
+
+float read_pitch_bend(unsigned int bend_up_pin, unsigned int bend_down_pin);
+
+#endif
